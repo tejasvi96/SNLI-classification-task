6e0c125b83ee3a67418651141375d45b002717c9
tejasvi96 <tejshar96@gmail.com>
2020-04-16 16:07:23 +0530

    added

--- main.py
+++ main.py
@@ -1,99 +1,52 @@
-from gensim.test.utils import datapath
-from gensim import utils
-import gensim.models
-import os
+import getopt, sys ,os
+import torch 
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import tensor,optim
+from torch.autograd import Variable
+import pickle
+import torchvision
+from torchvision.datasets import FashionMNIST
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import matplotlib.pyplot as plt
+from torchsummary import summary
+from scipy.sparse import hstack
+# from torch_utils import AverageMeter
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn import feature_extraction as fe
+import pickle
 from gensim.models import KeyedVectors
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
-from collections import Counter
-import nltk
-import pandas as pd
-import numpy as np
-import torch
 from torch.utils.data import TensorDataset, DataLoader
-import torch.nn as nn
-from torchsummary import summary
-import datetime
 import re
-root_path=os.getcwd()
-cwd=os.getcwd()
-
-glove_file = datapath(cwd+'//vectors_2.txt')
-tmp_file = get_tmpfile(cwd+"/test_word2vec_300_2.txt")
-_ = glove2word2vec(glove_file, tmp_file)
-pretrained_model = KeyedVectors.load_word2vec_format(tmp_file)
-test_file= r"snli_1.0_test.csv"
-d2=pd.read_csv(test_file)
-test_sent1=d2.sentence1
-test_sent2=d2.sentence2
-
-
-vocab_size=len(pretrained_model.wv.vocab)
-test_sentences_inp=[i for i in range(test_sent1.shape[0])]
-test_sentences_out=[i for i in range(test_sent2.shape[0])]
-for i, sentence in enumerate(test_sent1):
-    sentence=sentence.lower()
-    sentence=re.sub('[!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~]','',sentence)
-    test_sentences_inp[i] = [pretrained_model.wv.vocab.get(word).index if word in pretrained_model.wv.vocab.keys() else vocab_size-1 for word in sentence.split() ]
-    # test_sentences_inp[i].append(vocab_size-1)
-for i, sentence in enumerate(test_sent2):
-    sentence=sentence.lower()
-    sentence=re.sub('[!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~]','',sentence)
-    # Looking up the mapping dictionary and assigning the index to the respective words
-    test_sentences_out[i] = [pretrained_model.wv.vocab.get(word).index if word in pretrained_model.wv.vocab.keys() else vocab_size-1 for word in sentence.split() ]
-    # test_sentences_out[i].append(vocab_size-1)
-
-
-
-# Defining a function that either shortens sentences or pads sentences with 0 to a fixed length
-def pad_input(sentences1, seq_len):
-    features = np.zeros((len(sentences1), seq_len),dtype=int)
-    for i in range(len(sentences1)):
-      try:
-
-        sent_len=len(sentences1[i])
-        
-        if sent_len!=seq_len:
-          if sent_len<seq_len:
-            # print(np.array(sentences[i]+[0 for j in range(seq_len-sent_len)]))
-            features[i]=np.array(sentences1[i]+[vocab_size -1 for j in range(seq_len-sent_len)])
-          else:
-            features[i]=np.array(sentences1[i][:seq_len]  )
-        else:
-          features[i]=np.array(sentences1[i])
-      except:
-        print(sentences1[i])
-        pass
-    return features
-
 maxlen=50
-seq_len = maxlen  # The length that the sentences will be padded/shortened to
+vocab_size=31292
+device=torch.device('cpu')
 
-
-test_sentences_inp1 = pad_input(test_sentences_inp, seq_len)
-test_sentences_out1 = pad_input(test_sentences_out, seq_len)
-
-mapping={}
-mapping['neutral']=0
-mapping['contradiction']=1
-mapping['entailment']=2
-test_labels=[mapping[d2.label[i]] for i in range(len(d2.label))]
-test_labels=np.array(test_labels,dtype=float)
-
-batch_size = 128
-test_data1 = TensorDataset(torch.from_numpy(test_sentences_inp1),torch.from_numpy(test_sentences_out1), torch.from_numpy(test_labels))
-test_loader = DataLoader(test_data1, shuffle=True, batch_size=batch_size)
-
-is_cuda = torch.cuda.is_available()
-if is_cuda:
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
-
+def load_checkpoint(filepath):
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    model_path=os.path.join(__location__,filepath)
+    if os.path.isfile(model_path):
+        print("Model Already trained and File exists")
+        checkpoint = torch.load(filepath)
+        
+        model = checkpoint['model']
+        model.load_state_dict(checkpoint['state_dict'])
+        model.eval()
+        return model
+    else:
+        return Net()    
 from torch.autograd import Variable
 class LSTM_2(nn.Module):
-  def __init__(self, options): 
+  def __init__(self, options,pretrained_model): 
     super(LSTM_2, self).__init__()
     weights = torch.FloatTensor(pretrained_model.wv.vectors)
     self.embedding = nn.Embedding(options['vocab_size'], options['embed_dim']).from_pretrained(weights)
@@ -117,6 +70,9 @@ class LSTM_2(nn.Module):
   def forward(self, premise,hypothesis):
     # print(premise)
     # print(premise.shape)
+    global maxlen
+    global vocab_size
+    global device
     self.hidden=self.init_hidden()
     seq_len_prem=torch.zeros(premise.shape[0]).to(device)
     seq_len_hypo=torch.zeros(premise.shape[0]).to(device)
@@ -172,74 +128,218 @@ class LSTM_2(nn.Module):
 
   def init_hidden(self):
       # the weights are of the form (nb_layers, batch_size, nb_lstm_units)
-      hidden_a = torch.randn(2*2, 64,50)
-      hidden_b = torch.randn(2*2, 64,50)
+      hidden_a = torch.randn(2*2, 128,50)
+      hidden_b = torch.randn(2*2, 128,50)
       
       # hidden_b = torch.randn(self.hparams.nb_lstm_layers, self.batch_size, self.nb_lstm_units)
-
-      if is_cuda:
-        hidden_a = hidden_a.cuda()
-        hidden_b = hidden_b.cuda()
-
+        
+   
       hidden_a = Variable(hidden_a)
       hidden_b = Variable(hidden_b)
   def LSTM_2(options):
     return LSTM_2(options)
 
 
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
 
 
-dataset_options = {
-              'batch_size': batch_size, 
-              'device': device
-            }
+def test_deep(model, testloader):
+    """ Training the model using the given dataloader for 1 epoch.
+    Input: Model, Dataset, optimizer,
+    """
+    mapping2={}
+    mapping2[0]='neutral'
+    mapping2[1]='contradiction'
+    mapping2[2]='entailment'
+    n_correct, n_total, n_loss = 0, 0, 0
+    confusion_matrix = torch.zeros(3, 3)
+    model.eval()
+    avg_loss = AverageMeter("average-loss")
+    y_gt = []
+    y_pred_label = []
+    # hidden=model.init_hidden()
+    criterion = nn.CrossEntropyLoss()
+    global device
+    batch_size=128
+    with torch.no_grad():
+      for batch_idx, (premise,hypothesis,target) in enumerate(testloader):
+        premise=premise.to(device).to(torch.int64)
+        hypothesis=hypothesis.to(device).to(torch.int64)
+        target=target.to(device).to(torch.int64)
+        # _,target=target.max(dim=1)
+        answer = model(premise,hypothesis)
+        loss = criterion(answer, target.long())
 
+        pred = torch.max(answer, 1)[1].view(target.size())
+        for t, p in zip(target, pred):
+          confusion_matrix[t.long()][p.long()] += 1
+        
+        n_correct += (pred == target).sum().item()
+        n_total += batch_size
+        n_loss += loss.item()
+        y_gt+=list(mapping2[int(i)] for i in target.numpy())
+        y_pred_label+=list(mapping2[int(i)] for i in pred.numpy())
+      test_loss = n_loss/n_total
+      test_acc = 100. * n_correct/n_total
 
-output_size = 3
-embedding_dim = 300
-lr=0.0005
-vocab_size=len(pretrained_model.wv.vocab)
-hidden_dim = 512
-n_layers = 2
+    return test_loss, y_gt, y_pred_label
 
-model_options = {
-              'vocab_size': vocab_size, 
-              'embed_dim': embedding_dim,
-              'out_dim': output_size,
-              'dp_ratio':0.5,
-              'd_hidden':hidden_dim
-            }        
-model = LSTM_2(model_options)
-# hidden=model.init_hidden()
-criterion = nn.CrossEntropyLoss()
-opt = torch.optim.Adam(model.parameters(), lr = lr,weight_decay=1e-4)
+def gen_graph(gt,pred):
+    op = confusion_matrix(gt, pred)
+    print(op)
+    plt.figure(figsize=(10, 7))
+    fig = sn.heatmap(op, annot=True)
+    plt.title("Confusion Matrix for MLP")
+    plt.show()
+def test_logistic(model,train_data,test_data,test_labels):
+    train_data_mat=train_data[['sentence1','sentence2']].values
+    test_data_mat=test_data[['sentence1','sentence2']].values
+    vectorizer1 = fe.text.TfidfVectorizer( sublinear_tf=True, use_idf =True,analyzer='word',ngram_range=(1,2))
+    vectorizer2 = fe.text.TfidfVectorizer( sublinear_tf=True, use_idf =True,analyzer='word',ngram_range=(1,2))
 
-model.load_state_dict(torch.load(r"LSTM_c_best_v.pt" ))
-model.to(device)
+    train_sent1=vectorizer1.fit_transform(train_data_mat[:,0])
+    train_sent2=vectorizer2.fit_transform(train_data_mat[:,1])
+    test_sent1=vectorizer1.transform(test_data_mat[:,0])
+    test_sent2=vectorizer2.transform(test_data_mat[:,1])
+    test_data=hstack((test_sent1,test_sent2))
+    y_gt=test_labels
+    y_pred_label = []   
+    y_pred_label=model.predict(test_data)
+    
+    return y_gt,y_pred_label    
 
+    # Defining a function that either shortens sentences or pads sentences with 0 to a fixed length
+def pad_input(sentences1, seq_len):
+    features = np.zeros((len(sentences1), seq_len),dtype=int)
+    for i in range(len(sentences1)):
+        try:
+            sent_len=len(sentences1[i])
+            
+            if sent_len!=seq_len:
+                if sent_len<seq_len:
+                    # print(np.array(sentences[i]+[0 for j in range(seq_len-sent_len)]))
+                    features[i]=np.array(sentences1[i]+[vocab_size -1 for j in range(seq_len-sent_len)])
+                else:
+                    features[i]=np.array(sentences1[i][:seq_len]  )
+            else:
+                features[i]=np.array(sentences1[i])
+        except:
+            print(sentences1[i])
+            pass
+    return features    
+def preprocess(d2,pretrained_model):
+    global vocab_size
+    test_sent1=d2.sentence1
+    test_sent2=d2.sentence2
+    test_sentences_inp=[i for i in range(test_sent1.shape[0])]
+    test_sentences_out=[i for i in range(test_sent2.shape[0])]
+    for i, sentence in enumerate(test_sent1):
+        sentence=sentence.lower()
+        sentence=re.sub('[!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~]','',sentence)
+        test_sentences_inp[i] = [pretrained_model.wv.vocab.get(word).index if word in pretrained_model.wv.vocab.keys() else vocab_size-1 for word in sentence.split() ]
+        # test_sentences_inp[i].append(vocab_size-1)
+    for i, sentence in enumerate(test_sent2):
+        sentence=sentence.lower()
+        sentence=re.sub('[!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~]','',sentence)
+        # Looking up the mapping dictionary and assigning the index to the respective words
+        test_sentences_out[i] = [pretrained_model.wv.vocab.get(word).index if word in pretrained_model.wv.vocab.keys() else vocab_size-1 for word in sentence.split() ]
+        # test_sentences_out[i].append(vocab_size-1)
+    return test_sentences_inp,test_sentences_out    
+    
+def main():
+    cwd=os.getcwd()
+    test_file= cwd+r"/snli_1.0/snli_1.0/snli_1.0_test.csv"
+    train_file=cwd+r"/snli_1.0/snli_1.0/snli_1.0_train.csv"
+    d1=pd.read_csv(train_file)
+    d2=pd.read_csv(test_file)
 
-n_correct, n_total, n_loss = 0, 0, 0
-labels = test_labels
-confusion_matrix = torch.zeros(3, 3)
+    glove_file = datapath(cwd+'//vectors_2.txt')
+    tmp_file = get_tmpfile(cwd+"/test_word2vec_300_2.txt")
+    _ = glove2word2vec(glove_file, tmp_file)
+    pretrained_model = KeyedVectors.load_word2vec_format(tmp_file)
+    vocab_size=len(pretrained_model.wv.vocab)
 
-with torch.no_grad():
-  for batch_idx, (premise,hypothesis,target) in enumerate(test_loader):
-    premise=premise.to(device).to(torch.int64)
-    hypothesis=hypothesis.to(device).to(torch.int64)
-    target=target.to(device).to(torch.int64)
-    # _,target=target.max(dim=1)
-    answer = model(premise,hypothesis)
-    loss = criterion(answer, target.long())
+    test_sentences_inp,test_sentences_out=preprocess(d2,pretrained_model)
+    
+    global maxlen
+    seq_len = maxlen  # The length that the sentences will be padded/shortened to
+    
+    
+    test_sentences_inp1 = pad_input(test_sentences_inp, seq_len)
+    test_sentences_out1 = pad_input(test_sentences_out, seq_len)
+    
+    mapping={}
+    mapping['neutral']=0
+    mapping['contradiction']=1
+    mapping['entailment']=2
+    test_labels_1=[d2.label[i] for i in range(len(d2.label))]
+    test_labels=[mapping[d2.label[i]] for i in range(len(d2.label))]
+    test_labels=np.array(test_labels,dtype=float)
+    
+    batch_size = 128
+    dataset = TensorDataset(torch.from_numpy(test_sentences_inp1),torch.from_numpy(test_sentences_out1), torch.from_numpy(test_labels))
+    testloader = DataLoader(dataset, batch_size=128, shuffle=False)
 
-    pred = torch.max(answer, 1)[1].view(target.size())
-    for t, p in zip(target, pred):
-      confusion_matrix[t.long()][p.long()] += 1
+    # Hyperparams
+    output_size = 3
+    embedding_dim = 300
+    vocab_size=len(pretrained_model.wv.vocab)
+    hidden_dim = 512
+    n_layers = 3
 
-    n_correct += (pred == target).sum().item()
-    n_total += batch_size
-    n_loss += loss.item()
+    model_options = {
+                  'vocab_size': vocab_size, 
+                  'embed_dim': embedding_dim,
+                  'out_dim': output_size,
+                  'dp_ratio':0.5,
+                  'd_hidden':hidden_dim
+                }        
+    model = LSTM_2(model_options,pretrained_model)
+    model.load_state_dict(torch.load("./LSTM_c_best_v.pt" ))
 
-  test_loss = n_loss/n_total
-  test_acc = 100. * n_correct/n_total
-  
-print(test_loss, test_acc, confusion_matrix)
+    # from training_conv_net import LeNet
+    clf = LogisticRegression(random_state=0,max_iter=10000,multi_class='ovr')
+    filename="./Logistic_model.sav"
+    clf = pickle.load(open(filename, 'rb'))
+    loss, gt, pred = test_deep(model, testloader)
+    with open("deepmodel.txt", 'w') as f:
+        f.write("Loss on Test Data : {}\n".format(loss))
+        f.write("Accuracy on Test Data : {}\n".format(np.mean(np.array(gt) == np.array(pred))))
+        f.write("gt_label,pred_label \n")
+        for idx in range(len(gt)):
+            f.write("{},{}\n".format(gt[idx], pred[idx]))
+
+    # gen_graph(gt,pred)    
+    
+    gt, pred = test_logistic(clf, d1,d2,test_labels_1)
+    with open("tfidf.txt", 'w') as f:
+        f.write("Accuracy on Test Data : {}\n".format(np.mean(np.array(gt) == np.array(pred))))
+        f.write("gt_label,pred_label \n")   
+        for idx in range(len(gt)):
+            f.write("{},{}\n".format(gt[idx], pred[idx]))
+    # gen_graph(gt,pred)
+    
+if __name__ == "__main__":
+    main()
